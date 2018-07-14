3e9e8335da6d2225161b50d1a0954b7f14babd8f
Neel Patel <patelneel55@gmail.com>
2018-07-14 16:57:30 -0500

    Remove excess URL components if present

--- send.sh
+++ send.sh
@@ -54,7 +54,7 @@ COMMIT_TIME="$(git log -1 "${GIT_COMMIT}" --pretty="%ct")"
 
 GIT_URL=$(git config --get remote.origin.url)
 JENKINS_REPO_SLUG=${GIT_URL#*"github.com/"}
-
+JENKINS_REPO_SLUG=${JENKINS_REPO_SLUG%".git"*}
 
 if [ "$AUTHOR_NAME" == "$COMMITTER_NAME" ]; then
   CREDITS="$AUTHOR_NAME authored & committed"
